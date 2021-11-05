b800a0b768c165fdfc7fbd5e676a425ce9f6cad2
Rijo George <rijothedeveloper@gmail.com>
2021-11-05 11:52:51 -0700

    calculating payment done

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -21,17 +21,27 @@ function getCurrentUIValues() {
 // Put some default values in the inputs
 // Call a function to calculate the current monthly payment
 function setupIntialValues() {
+
 }
 
 // Get the current values from the UI
 // Update the monthly payment
 function update() {
+  const values = getCurrentUIValues();
+  const payment = calculateMonthlyPayment(values);
+  document.getElementById("monthly-payment").innerText = payment;
 }
 
 // Given an object of values (a value has amount, years and rate ),
 // calculate the monthly payment.  The output should be a string
 // that always has 2 decimal places.
 function calculateMonthlyPayment(values) {
+  // return (values.amount * (values.rate / 12)) / (1 - Math.pow( (1 + (values.rate / 12)), (values.years * -1) ));
+  const a = values.amount;
+  const r = (values.rate / 10)/12;
+  const n = values.years * 12;
+  const pay =  a / (((Math.pow((1+r),n) - 1)) / (Math.pow((1+r),n) * r));
+  return pay;
 }
 
 // Given a string representing the monthly payment value,
